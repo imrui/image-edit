d8fb1d45296f550897d18720409fc0650ee138b1
imrui <hilo.sky.do@gmail.com>
2016-10-17 15:27:23 +0800

    调整页面渲染逻辑，完成移除角标功能。

--- handlers/image_handler.py
+++ handlers/image_handler.py
@@ -3,6 +3,7 @@ from handlers.base_handler import BaseHandler
 import os
 import utils
 import zipfile
+import traceback
 
 
 class AppIconSetHandler(BaseHandler):
@@ -14,41 +15,44 @@ class AppIconSetHandler(BaseHandler):
 class IconMergeHandler(BaseHandler):
 
     def get(self, *args, **kwargs):
-        app_os = self.get_argument('os', '')
-        icon = self.get_argument('icon', '')
-        subscript = self.get_argument('subscript', '')
-        icon_set = self.app_icon_set.get(app_os)
-        if not icon_set or not icon:
-            self.render_obj(dict(code=404, msg='arguments error'))
-            return
-        img_icon, fp_icon = utils.get_upload_image_file(self.upload_path, 'icon', icon)
-        img_sub, fp_sub = None, ''
-        if not img_icon:
-            self.render_obj(dict(code=404, msg='image file not found'))
-            return
-        if subscript:
-            img_sub, fp_sub = utils.get_upload_image_file(self.upload_path, 'subscript', subscript)
-            if not img_sub:
+        try:
+            app_os = self.get_argument('os', '')
+            icon = self.get_argument('icon', '')
+            subscript = self.get_argument('subscript', '')
+            icon_set = self.app_icon_set.get(app_os)
+            if not icon_set or not icon:
+                self.render_obj(dict(code=400, msg='arguments error'))
+                return
+            img_icon, fp_icon = utils.get_upload_image_file(self.upload_path, 'icon', icon)
+            img_sub, fp_sub = None, ''
+            if not img_icon:
                 self.render_obj(dict(code=404, msg='image file not found'))
                 return
-        new_img = utils.img_composite(img_icon, img_sub) if img_sub else img_icon
-        fp = '%s-%s' % (fp_icon, fp_sub) if fp_sub else fp_icon
-        app_os_path = os.path.join(self.media_path, 'icons', app_os)
-        new_img_path = os.path.join(app_os_path, fp)
-        print(new_img_path)
-        for d in icon_set:
-            size = d.get('size', '')
-            filename = d.get('filename', '')
-            if not filename or not size:
-                continue
-            d_img = utils.img_resize(new_img, size)
-            if not d_img:
-                continue
-            utils.img_save(d_img, new_img_path, filename)
-        # 生成所有icon的zip文件
-        zf_name = os.path.join(app_os_path, '%s.zip' % fp)
-        zf = zipfile.ZipFile(zf_name, 'w', zipfile.ZIP_DEFLATED)
-        for fn in os.listdir(new_img_path):
-            zf.write(os.path.join(new_img_path, fn), os.path.join(fp, fn))
-        zf.close()
-        self.render_obj(dict(code=200, msg='success', fp=fp))
+            if subscript:
+                img_sub, fp_sub = utils.get_upload_image_file(self.upload_path, 'subscript', subscript)
+                if not img_sub:
+                    self.render_obj(dict(code=404, msg='image file not found'))
+                    return
+            new_img = utils.img_composite(img_icon, img_sub) if img_sub else img_icon
+            fp = '%s-%s' % (fp_icon, fp_sub) if fp_sub else fp_icon
+            app_os_path = os.path.join(self.media_path, 'icons', app_os)
+            new_img_path = os.path.join(app_os_path, fp)
+            for d in icon_set:
+                size = d.get('size', '')
+                filename = d.get('filename', '')
+                if not filename or not size:
+                    continue
+                d_img = utils.img_resize(new_img, size)
+                if not d_img:
+                    continue
+                utils.img_save(d_img, new_img_path, filename)
+            # 生成所有icon的zip文件
+            zf_name = os.path.join(app_os_path, '%s.zip' % fp)
+            zf = zipfile.ZipFile(zf_name, 'w', zipfile.ZIP_DEFLATED)
+            for fn in os.listdir(new_img_path):
+                zf.write(os.path.join(new_img_path, fn), os.path.join(fp, fn))
+            zf.close()
+            self.render_obj(dict(code=200, msg='success', fp=fp))
+        except:
+            traceback.print_exc()
+            self.render_obj(dict(code=500, msg='system error'))
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,16 +1,5 @@
 var imageControllers = angular.module('imageControllers', []);
 
-// var get_file_path = function (file_info) {
-//   if (!file_info) {
-//     return ''
-//   }
-//   var info = file_info.split('/');
-//   if (info.length != 2) {
-//     return ''
-//   }
-//   return info[0]
-// };
-
 imageControllers.controller('IconCtrl', ['$scope', '$log', 'IconMerge',
   function ($scope, $log, IconMerge) {
     $scope.osOptions = [
@@ -18,26 +7,33 @@ imageControllers.controller('IconCtrl', ['$scope', '$log', 'IconMerge',
       {id: 'ios', name: "Ios"}
     ];
     $scope.info = {os: $scope.osOptions[0]};
+    var osType = $scope.getImgCookies('osType');
+    if (osType) {
+      for (var i=0; i<$scope.osOptions.length; i++) {
+        if ($scope.osOptions[i].id == osType) {
+          $scope.info.os = $scope.osOptions[i];
+          break;
+        }
+      }
+    }
     $scope.refreshImage('icon');
     $scope.refreshImage('subscript');
+    var clearIconsPath = function () {
+        $scope.removeImgCookies('icons-' + $scope.osOptions[0].id);
+        $scope.removeImgCookies('icons-' + $scope.osOptions[1].id);
+        $scope.iconsPath = '';
+    };
     angular.element('.cls-file-upload').fileupload({
       add: function (e, data) {
         data.submit();
       },
       done: function (e, data) {
         $scope.afterUpload(data.result);
-        $scope.removeImgCookies('icons-' + $scope.osOptions[0].id);
-        $scope.removeImgCookies('icons-' + $scope.osOptions[1].id);
-        $scope.iconsPath = '';
+        clearIconsPath();
         $scope.$apply();
       }
     });
     var refreshIcons = function () {
-      // if ($scope.subscript) {
-      //   $scope.iconsPath = get_file_path($scope.icon) + '-' + get_file_path($scope.subscript);
-      // } else {
-      //   $scope.iconsPath = get_file_path($scope.icon);
-      // }
       $scope.iconsPath = $scope.getImgCookies('icons-' + $scope.info.os.id);
     };
     $scope.changeOsType = function () {
@@ -45,12 +41,21 @@ imageControllers.controller('IconCtrl', ['$scope', '$log', 'IconMerge',
         $scope.iconSet = $scope.appIconSet[$scope.info.os.id];
       }
       refreshIcons();
+      $scope.setImgCookies('osType', $scope.info.os.id);
     };
     $scope.$on('on_finish_app_icon_set', function () {
       $scope.changeOsType();
     });
     $scope.changeOsType();
     // refreshIcons();
+    $scope.delSubscript = function () {
+      if (!$scope.subscript) {
+        return;
+      }
+      $scope.subscript = '';
+      $scope.removeImgCookies('subscript');
+      clearIconsPath();
+    };
     $scope.iconMerge = function () {
       if (!$scope.icon) {
         alert('请上传icon');
@@ -63,6 +68,19 @@ imageControllers.controller('IconCtrl', ['$scope', '$log', 'IconMerge',
       $log.debug(param);
       IconMerge.get(param, function (data) {
         $log.debug(data);
+        if (data.code == 404) {
+          clearIconsPath();
+          $scope.icon = '';
+          $scope.subscript = '';
+          $scope.removeImgCookies('icon');
+          $scope.removeImgCookies('subscript');
+          alert('icon或角标已失效，请重新上传！');
+          return;
+        }
+        if (data.code != 200) {
+          alert(data.code + ': ' + data.msg);
+          return;
+        }
         $scope.setImgCookies('icons-' + param.os, data.fp);
         refreshIcons();
       });
